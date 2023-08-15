3d69840f123572d7a4fd9ac22d0ea5c8a0d64e4f
evgen122 <evg.khristenko@gmail.com>
2023-08-15 15:39:52 +0300

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function timeVideo(obj) {
+  const time = obj.seconds;
+  //   console.log(time);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+}
+
+const secondsVideo = localStorage.getItem('videoplayer-current-time');
+// console.log(secondsVideo);
+
+player.on('timeupdate', throttle(timeVideo, 1000));
+player.setCurrentTime(secondsVideo);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+const elements = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[name=email]'),
+  message: document.querySelector('textarea[name=message]'),
+  submit: document.querySelector('button[type=submit]'),
+};
 
+const throttle = require('lodash.throttle');
+const objInfo = {};
+const objData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+// console.log(objData);
+// console.log(objData.email);
+// console.log(objData.message);
+
+if (objData) {
+  elements.email.value = objData.email ?? '';
+  elements.message.value = objData.message ?? '';
+}
+
+elements.form.addEventListener('input', throttle(hendllerForm, 500));
+elements.submit.addEventListener('click', hendllerSubmit);
+
+function hendllerForm() {
+  objInfo.email = elements.email.value;
+  objInfo.message = elements.message.value;
+
+  // console.log(objInfo);
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(objInfo));
+}
+
+function hendllerSubmit(event) {
+  event.preventDefault();
+
+  const info = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(info);
+
+  localStorage.removeItem('feedback-form-state');
+  elements.form.reset();
+}
